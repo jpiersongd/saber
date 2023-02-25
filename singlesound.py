fbef79ec869c91aa53fd60320ffd63110cb96227
jpiersongd <jemzpierson@msn.com>
2023-02-25 11:14:54 -0800

    new singlesound

--- singlesound.py
+++ singlesound.py
@@ -5,55 +5,33 @@ import numpy
 
 
 # Initialize the audio
-pygame.mixer.pre_init(44100, -16, 16, 2048)   #frequency, size, channels, buffer
+pygame.mixer.pre_init(44100, -16, 2, 2048)   #frequency, size, channels, buffer
 pygame.mixer.init()
 print("pygame mixer started")
 
 
-# Define the sound play function
-def play_sound(): 
-    volume = 1    
-    frequency1 = 46
 
-    
-    #print('freqA={0:0.2F} freqB={1:0.2F} freqC={2:0.2F} freqD={3:0.2F} \t'.format(frequency1, frequency2, frequency3, frequency4))
-
-    channel1 = pygame.mixer.Channel(0)    
-    sound1 = pygame.sndarray.make_sound(create_sound(frequency1, volume))
-    channel1.play(sound1, loops=-1)
-
-
-
-#Define the sound creation function
-def create_sound(frequency, volume):
-    duration = 160
+# Define the sound creation function
+def create_sound(frequency1, frequency2, volume):
+    duration = 5000
     sample_rate = 44100
     num_samples = int(duration * sample_rate / 1000)
 
     sound_data = []
     for i in range(num_samples):
         t = float(i) / sample_rate
-        sound_sample = float(volume * math.sin(frequency * t * 2 * math.pi))
-        #print(sound_sample)
-        sound_data.append(sound_sample * 32767)
+        sound_sample1 = float(volume * numpy.sin(frequency1 * t * 2 * numpy.pi))
+        sound_sample2 = float(volume * numpy.sin(frequency2 * t * 2 * numpy.pi))
+        sound_data.append([sound_sample1, sound_sample2])
         
-    # Ensure that the length of sound_data is evenly divisible by 2
-    while len(sound_data) % 2 != 0:
-        sound_data.append(0)
-    
     sound_array = numpy.array(sound_data, dtype=numpy.int16)
-    sound_array = numpy.reshape(sound_array, (-1, 2))
-    #print(sound_array)
-    
     return sound_array
 
+# Create a stereo sound with 2 channels
+frequency1 = 440  # Hz
+frequency2 = 880  # Hz
+volume = 0.5
+stereo_sound = create_sound(frequency1, frequency2, volume)
 
-# Start the main loop
-is_recording = True
-while True:
-    if is_recording:
-        # Play the sound
-        play_sound()
-
-    # Delay for a short time
-    time.sleep(0)
+# Print the shape of the stereo sound array
+print(stereo_sound.shape)  # Output: (7056, 2)
