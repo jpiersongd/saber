77b1d082d9ee7a2649378244f72b69ad40baa888
jpiersongd <jemzpierson@msn.com>
2023-02-27 21:27:35 -0800

    none

--- soundstream.py
+++ soundstream.py
@@ -1,5 +1,4 @@
 import time
-
 import numpy as np
 import pyaudio
 
@@ -8,13 +7,20 @@ p = pyaudio.PyAudio()
 volume = 0.5  # range [0.0, 1.0]
 fs = 44100  # sampling rate, Hz, must be integer
 duration = 5.0  # in seconds, may be float
-f = 440.0  # sine frequency, Hz, may be float
+f1 = 440.0  # sine frequency, Hz, may be float
+f2 = 128.0  # second sine frequency, Hz, may be float
 
-# generate samples, note conversion to float32 array
-samples = (np.sin(2 * np.pi * np.arange(fs * duration) * f / fs)).astype(np.float32)
+# generate samples for first frequency, note conversion to float32 array
+samples1 = (np.sin(2 * np.pi * np.arange(fs * duration) * f1 / fs)).astype(np.float32)
+
+# generate samples for second frequency
+samples2 = (np.sin(2 * np.pi * np.arange(fs * duration) * f2 / fs)).astype(np.float32)
 
 # per @yahweh comment explicitly convert to bytes sequence
-output_bytes = (volume * samples).tobytes()
+output_bytes1 = (volume * samples1).tobytes()
+
+# convert samples for second frequency to bytes
+output_bytes2 = (volume * samples2).tobytes()
 
 # for paFloat32 sample values must be in range [-1.0, 1.0]
 stream = p.open(format=pyaudio.paFloat32,
@@ -22,9 +28,14 @@ stream = p.open(format=pyaudio.paFloat32,
                 rate=fs,
                 output=True)
 
-# play. May repeat with different volume values (if done interactively)
+# play first frequency. May repeat with different volume values (if done interactively)
 start_time = time.time()
-stream.write(output_bytes)
+stream.write(output_bytes1)
+print("Played sound for {:.2f} seconds".format(time.time() - start_time))
+
+# update stream with second frequency
+start_time = time.time()
+stream.write(output_bytes2)
 print("Played sound for {:.2f} seconds".format(time.time() - start_time))
 
 stream.stop_stream()
